396e7b17f17bd1d50d9922d26c09e4b75f3292c9
yaakovizel <z7685727@gmail.com>
2026-06-23 12:42:45 +0300

    added practic 4,8

--- array_metods.js
+++ array_metods.js
@@ -4,3 +4,13 @@ const duble = nums.map((num) => num * 2);
 console.log(duble);
 
 // 2
+const strings = ["hello", "world"];
+strings.forEach((string, index) => {
+  strings[index] = string.toUpperCase();
+});
+
+// 8
+const prices = [100, 120, 50, 87, 180];
+const result = prices
+  .filter((price) => price > 100)
+  .map((price) => price * 1.1);
