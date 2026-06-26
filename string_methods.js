afe0a621957c9f8d04df1e764f831ae00dc535b4
yaakovizel <z7685727@gmail.com>
2026-06-26 15:59:09 +0300

    added string methods

---
+++ string_methods.js
@@ -0,0 +1,88 @@
+// // 1
+// const str = " hello world ";
+// const strip = str.trim();
+// console.log(str);
+// console.log(strip);
+
+// // 2
+// const email = "user@gmail.com";
+// console.log(email.includes("@"));
+
+// // 3
+// const str1 = "Javascript";
+// const upper = str1.toUpperCase();
+// console.log(upper);
+
+// // 4
+// const str2 = "HELLO WORLD";
+// const lower = str2.toLowerCase();
+// console.log(lower);
+
+// // 6
+// const str3 = "javaScript";
+// const sliced = str3.slice(0, 4);
+// console.log(sliced);
+
+// // 7
+// const str = "javaScript";
+// const sliced = str.slice(4);
+// console.log(sliced);
+
+// // 8
+// const str = "https://example.com";
+// console.log(str.startsWith("https"));
+
+// // 9
+// const str = "mywebsite.org";
+// console.log(str.endsWith(".com"));
+
+// // 10
+// const str1 = "hello world";
+// const replaced = str1.replace("hello", "hey");
+// console.log(replaced);
+
+// // 11
+// const str = "banana";
+// const replaced = str.replaceAll("a", "o");
+// console.log(replaced);
+
+// // 12
+// const str2 = "one two three";
+// const arr = str2.split(" ");
+// console.log(arr);
+
+// // 13
+// const str2 = "apple,banana,grape";
+// const arr = str2.split(",", 2);
+// console.log(arr);
+
+// // 14
+// const str = "banana";
+// console.log(str.indexOf("a"));
+
+// // 15
+// const str = "banana";
+// console.log(str.lastIndexOf("a"));
+
+// // 18
+// const str = "ha";
+// console.log(str.repeat(3));
+
+// // 19
+// const str = "Hello";
+// console.log(str.charAt(0));
+
+// // 20
+// const str = "Hello";
+// console.log(str.at(-1));
+
+// // 21
+// const str = "Hello";
+// const str2 = "world";
+// const result = str.concat(" ", str2);
+// console.log(result);
+
+// // 22
+// const str = "JavaScript";
+// const sliced = str.substring(2, 6);
+// console.log(sliced);
